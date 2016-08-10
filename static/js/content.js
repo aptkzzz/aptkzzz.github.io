d5eef474e72c7b4f9053e67eb385f636b58db8fd
arseniypetrikor <arseniy.beresnev@gmail.com>
2016-08-10 11:44:06 +0500

    Design improvements
    Changed :hover opacity of .clickable images

--- static/js/content.js
+++ static/js/content.js
@@ -95,7 +95,7 @@ let pushTalkList = () => {
 			talk.classList.add("col-lg-offset-0");
 		}
 		let list = [];
-		list[0] = "<img class='img-rounded img-responsive' src='" + i.picture + "' onclick='redirect(\"" + i.link + "\")'/>";
+		list[0] = "<img class='img-rounded img-responsive clickable' src='" + i.picture + "' onclick='redirect(\"" + i.link + "\")'/>";
 		// list[1] = "<button class='btn btn-default' onclick='redirect(\"" + i.link + "\")'>" + i.name + "</button>";
 		talk.innerHTML = list[0]/* + list[1];*/
 		talkRow.appendChild(talk);
@@ -116,7 +116,7 @@ let pushWorkList = () => {
 			work.classList.add("col-sm-offset-2");
 		}
 		let list = [];
-		list[0] = "<img class='img-rounded img-responsive' src='" + i.picture + "' onclick='redirect(\"" + i.link + "\")' />";
+		list[0] = "<img class='img-rounded img-responsive clickable' src='" + i.picture + "' onclick='redirect(\"" + i.link + "\")' />";
 		// list[1] = "<button class='btn btn-default' onclick='redirect(\"" + i.link + "\")'>" + i.name + "</button>";
 		work.innerHTML = list[0]/* + list[1];*/
 		workRow.appendChild(work);
