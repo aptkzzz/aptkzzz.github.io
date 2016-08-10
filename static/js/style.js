6cfa6c56e01a01c6a62874a8b822b40c7b8f4af1
arseniypetrikor <arseniy.beresnev@gmail.com>
2016-08-11 00:16:16 +0500

    Dark theme bugfix

--- static/js/style.js
+++ static/js/style.js
@@ -5,7 +5,7 @@ let setDarkTheme = () => {
 		document.body.style.backgroundImage = "url('http://80.78.241.238:8192/img/bg/dark.png')";
 		navbar = document.querySelector(".navbar-fixed-top");;
 		navbar.classList.remove("navbar-default");
-		navbar.classList.add("navbar-inversed");
+		navbar.classList.add("navbar-inverse");
 		document.body.style.color = "white";
 	}
 }
